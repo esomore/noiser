a9e464fb5ecb0c0cb09c2458e232e6418d451f2f
Itamar Perez <itamarperez@gmail.com>
2024-04-27 18:34:54 -0700

    Add lowpass filter to remove noise in audio recordings

--- noiser.py
+++ noiser.py
@@ -8,6 +8,8 @@ from threading import Thread
 from datetime import datetime
 from flask import send_file
 import time
+from scipy.signal import butter, lfilter
+
 app = Flask(__name__)
 
 # Create a SQLite database and a table to store the recordings
@@ -54,13 +56,26 @@ def get_recordings():
     conn.close()  # Close the connection when you're done with it
     return jsonify(recordings)
 
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
 def record_audio():
     chunk = 2048
     sample_format = pyaudio.paInt16
-    channels = 1  # Change this to 1
+    channels = 1
     fs = 22050
+    cutoff = 300.0  # Desired cutoff frequency of the filter, Hz
     threshold = 50
-    record_time = 10  # Duration to record after detecting noise
+    record_time = 10
 
     p = pyaudio.PyAudio()
 
@@ -76,6 +91,8 @@ def record_audio():
 
     while True:
         data = stream.read(chunk)
+        data = np.fromstring(data, 'Int16')
+        data = butter_lowpass_filter(data, cutoff, fs)
         rms = audioop.rms(data, 2)
 
         if not recording and rms > threshold:
@@ -83,7 +100,7 @@ def record_audio():
             recording = True
             start_time = time.time()
             frames.append(data)
-            print('Noise detected')  # Print a message when a noise is detected
+            print('Noise detected')
         elif recording:
             if time.time() - start_time < record_time:
                 frames.append(data)
